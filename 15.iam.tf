215382af4ac307ba775396409c149fefbc0bf4b9
Manohar-1305 <maddy98989@gmail.com>
2025-03-01 23:15:30 +0530

    Update 15.iam.tf

--- 15.iam.tf
+++ 15.iam.tf
@@ -1,9 +1,5 @@
 
-# IAM Instance Profile for EC2 Access
-#data "aws_iam_instance_profile" "s3_access_profile" {
-#  name = "eks-role-bastion" # Replace with the actual name of your IAM instance profile
-#}
 data "aws_iam_instance_profile" "s3_access_profile" {
-  name = "eks-bastion"  # Ensure the exact name
+  name = "eks-bastion"
 }
 
