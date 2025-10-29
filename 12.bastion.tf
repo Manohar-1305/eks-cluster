86d52c45d0b490ad61e2d61645de8b816ede63a0
Manohar-1305 <maddy98989@gmail.com>
2025-10-29 19:46:21 +0530

    Update 12.bastion.tf

--- 12.bastion.tf
+++ 12.bastion.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type = var.instance_type
   subnet_id = aws_subnet.eks_public_subnet.id
   key_name = "testing-dev-1"
-  iam_instance_profile = data.aws_iam_instance_profile.s3_access_profile.name
+  # iam_instance_profile = data.aws_iam_instance_profile.s3_access_profile.name
   user_data            = file("bastion.sh")
   vpc_security_group_ids = [
     aws_security_group.bastion_sg.id
@@ -15,4 +15,5 @@ resource "aws_instance" "bastion" {
   associate_public_ip_address = true
   
   depends_on = [aws_eks_cluster.eks_cluster]
+
 }
